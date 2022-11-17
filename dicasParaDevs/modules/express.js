85273a97311ed714310e548c0882035d0a880cbe
Wanderley Cabral <wanderleycabral@iMac-de-Wanderley.local>
2022-11-17 20:01:07 -0300

    Atualizando database

--- dicasParaDevs/modules/express.js
+++ dicasParaDevs/modules/express.js
@@ -5,26 +5,31 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/home", (req, res) => {
-  res.contentType("application/html");
-  res.status(200).send("<h1> Hello Word!</h1>");
+// app.get("/home", (req, res) => {
+//   res.contentType("application/html");
+//   res.status(200).send("<h1> Hello Word!</h1>");
+// });
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+
+    res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 });
 
-app.get("/users", (req, res) => {
-  const users = [
-    {
-      name: "John Doe",
-      email: "john@doe.com",
-    },
-    {
-      name: "Jane Doe",
-      email: "jane@doe.com",
-    },
-  ];
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
 
-  res.status(200).json(users);
+    const user = await UserModel.findById(id);
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 });
-
 app.post("/users", async (req, res) => {
   try {
     const user = await UserModel.create(req.body);
@@ -35,6 +40,15 @@ app.post("/users", async (req, res) => {
   }
 });
 
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 const port = 8080;
 
-app.listen(port, () => console.log(`Rodando com express na porta  ${port}!`));
+app.listen(port, () => console.log(`Rodando com express na porta   ${port}!`));
