c4cf8356ef335fb7fe7367220201cee435388e8f
Wanderley Cabral <wanderleycabral@iMac-de-Wanderley.local>
2022-11-17 14:32:49 -0300

    update file

--- dicasParaDevs/index.js
+++ dicasParaDevs/index.js
@@ -1,13 +1,17 @@
-const { Person } = require("./person");
+// const { Person } = require("./person");
 const dotenv = require("dotenv");
 
+const connectToDatabase = require("./src/database/connect");
+
 dotenv.config();
+
+connectToDatabase();
 
 //require ("./modules/path.js");
 // require ("./modules/fs.js");
 // require ("./modules/http.js");
-require("./modules/express.js");
+// require("./modules/express.js");
 
-const person = new Person("Wanderley Cabral");
+// const person = new Person("Wanderley Cabral");
 
 // console.log(person.sayMyName());
