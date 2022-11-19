49eb66e3a4cdc907da13c110bb7df12bcb7fd879
Wanderley Cabral <wanderleycabral@iMac-de-Wanderley.local>
2022-11-19 17:51:55 -0300

    Atualizando database - DELETE

--- dicasParaDevs/modules/express.js
+++ dicasParaDevs/modules/express.js
@@ -49,6 +49,17 @@ app.patch("/users/:id", async (req, res) => {
     res.status(500).send(error.message);
   }
 });
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndRemove(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 const port = 8080;
 
 app.listen(port, () => console.log(`Rodando com express na porta   ${port}!`));
