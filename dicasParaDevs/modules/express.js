4771e4830e4a9dd5f1dc42bf21028ecfff98533b
Wanderley Cabral <wanderleycabral@iMac-de-Wanderley.local>
2022-11-19 18:54:15 -0300

    Conhecendo o EJS (View Engine)

--- dicasParaDevs/modules/express.js
+++ dicasParaDevs/modules/express.js
@@ -5,12 +5,19 @@ const app = express();
 
 app.use(express.json());
 
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+
 app.use((req, res, next) => {
   console.log(`Request Type: ${req.method}`);
   console.log(`Content Type: ${req.headers["content-type"]}`);
   console.log(`Date: ${new Date()}`);
   next();
-  //res.send("");
+});
+
+app.get("/views/users", async (req, res) => {
+  const users = await UserModel.find({});
+  res.render("index");
 });
 
 // app.get("/home", (req, res) => {
